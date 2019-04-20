9dbab69958bc7e1e1fb9e8d763000d10eb617308
Oscar Torres <40122963+JossueTorres@users.noreply.github.com>
2019-04-20 17:11:19 -0600

    Moficiar Edificios y footer
    -Quité un menú oculto del footer, porque molestaba el responsive(igual no lo ibamos a usar)
    -Agregué el esqueleto de ejemplo para edificios.

--- TourUtec_Admin/application/views/Edificios.php
+++ TourUtec_Admin/application/views/Edificios.php
@@ -1,9 +1,79 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
+<div class="content-header">
+<h1>Edificios&nbsp;<span class="ion ion-android-home"></span></h1>
+</div>
+<div class="content">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="box box-success">
+				<form name="formFil" action="" method="post">				
+					<div class="box-header with-border">
+						<h4>Filtros</h4>
+					</div>
+					<div class="box-body">
+						<div class="row">
+							<div class="col-sm-3">
+							<label for="txtNombre">Nombre:</label>
+								<input id="txtNombre" type="text" class="form-control txtNombre" placeholder="Nombre">
+							</div>
+							<div class="col-sm-3">
+							<label for="txtNombre">Acrónimo:</label>
+								<input id="txtAcronimo" type="text" class="form-control txtAcronimo" placeholder="Acrónimo">
+							</div>
+						</div>		
+					</div>
+					<div class="box-footer with-border">
+						<div class="pull-right">
+							<button role="button" class="btn btn-danger">Limpiar</button>
+							<button role="button" class="btn btn-primary"><span class="ion ion-search"></span>&nbsp;Buscar</button>				
+						</div>					
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="col-sm-12">
+			<div class="box box-primary">
+				<div class="box-header">
+					<button id="btnNuevo" class="btn btn-success"><span class="ion ion-plus"></span>&nbsp;Agregar</button>
+				</div>
+				<div class="box-body">
+					<table class="table table-striped table-responsive">
+						<thead>
+							<tr>
+								<th>Colum1</th>
+								<th>Colum2</th>
+								<th>Column3</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Text1</td>
+								<td>Text2</td>
+								<td>Text3</td>
+							</tr>
+							<tr>
+								<td>Text1</td>
+								<td>Text2</td>
+								<td>Text3</td>
+							</tr>
+							<tr>
+								<td>Text1</td>
+								<td>Text2</td>
+								<td>Text3</td>
+							</tr>
+							<tr>
+								<td>Text1</td>
+								<td>Text2</td>
+								<td>Text3</td>
+							</tr>
+							<tr>
+								<td>Text1</td>
+								<td>Text2</td>
+								<td>Text3</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>			
+		</div>	
+	</div>
+</div>
